54fe02ee157822bc64e86224c5387033bdbd43a1
kima <omer@paymeservice.com>
2024-03-01 21:50:49 +0200

    Added tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,5 +47,14 @@
                 </div>
             </main>
         </div>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RKGVRPNJWV"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-RKGVRPNJWV');
+        </script>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,4 +63,13 @@
     </section>
     <!-- Section: Design Block -->
     </body>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RKGVRPNJWV"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-RKGVRPNJWV');
+    </script>
 </html>
