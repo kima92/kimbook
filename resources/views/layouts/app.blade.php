63d4045b0174b8ff0315f06307a847de6f9e9f65
kima <omer@paymeservice.com>
2024-04-24 14:00:39 +0300

    Added microsoft clarity integration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,14 @@
         <link href="/css/book.css" rel="stylesheet">
 
         {{ \App\Utils\Vite::compile() }}
+
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "m1krx3jy7p");
+        </script>
     </head>
     <body class="font-sans antialiased" style="background-color: #0b1012">
         <div class="min-h-screen bg-gray-100">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,14 @@
         <script type="text/javascript" src="/js/turnjs4/zoom.min.js"></script>
 
         {{ \App\Utils\Vite::compile() }}
+
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "m1krx3jy7p");
+        </script>
     </head>
     <body class="antialiased">
     <!-- Section: Design Block -->
